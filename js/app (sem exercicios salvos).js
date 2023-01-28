e3368af3ac89d7a41c4b182be967fe5083c3ba40
Mariana M. Macêdo <eng.marianammacedo@gmail.com>
2023-01-28 06:21:17 -0300

    decimo-quarto commit

--- js/app (sem exercicios salvos).js
+++ js/app (sem exercicios salvos).js
@@ -11,6 +11,7 @@ const $btnAdicionarExercicio = document.querySelector('#btn-adicionar-exercicio'
 const $btnSalvarTreino = document.querySelector('#btn-salvar-treino');
 const $form = document.querySelector('form');
 
+
 const $infoNomeTreino = $form.querySelector('#info-nome-treino');
 const $infoDescricao = $form.querySelector('#info-descricao');
 const $infoNomeExercicio = $form.querySelector('#info-nome-exercicio');
@@ -24,8 +25,10 @@ const $infosExercicios = [$infoNomeExercicio, $infoSeries, $infoRepeticoes, $inf
 const $allInfos = document.querySelectorAll('.info');
 
 let treinos = JSON.parse(localStorage.getItem('treinos')) || [];
-let exercicios = JSON.parse(localStorage.getItem('exercicios')) || [];
 
+criarTreinos(treinos);
+
+let exercicios = [];
 const id = (arg) => {
     return arg.length + 1
 }
@@ -49,8 +52,7 @@ const $inputsTreino = [$inputNomeTreino, $inputDescricao];
 const $inputsExercicios = [$inputNomeExercicio, $inputSeries, $inputRepeticoes, $inputCarga, $inputDuracao, $inputIntervalo, $inputObservacao];
 const $allInputs = document.querySelectorAll('input');
 
-criarTreinos(treinos);
-renderizarInfo();
+// let topoTreinoDefinido = false;
 
 $form.addEventListener('keyup', (e) => {
     switch (e.target.id) {
@@ -87,7 +89,7 @@ $form.addEventListener('keyup', (e) => {
             $infoIntervalo.innerHTML = `${$inputIntervalo.value ?
                 `<i class="fa-solid fa-hand hand-icon"></i>
                 <span class="info-value">${$inputIntervalo.value}</span> seg.` : ''}`;
-
+        
         case 'observacao':
             $infoObservacao.innerHTML = `${$inputObservacao.value ?
                 `<i class="fa-solid fa-triangle-exclamation triangle-exclamation-icon"> </i>
@@ -160,21 +162,14 @@ $btnAdicionarExercicio.addEventListener('click', (e) => {
     if ($inputNomeTreino.value && $inputNomeExercicio.value) {
 
         $inputNomeTreino.setAttribute('disabled', '')
-        localStorage.setItem('treino', JSON.stringify($inputNomeTreino.value));
 
         if (exercicios.length === 0) $olExercicios.classList.remove('d-none');
 
-        if ($inputDescricao.value) {
-            $inputDescricao.setAttribute('disabled', '');
-            localStorage.setItem('descricao', JSON.stringify($inputDescricao.value));
-        }
+        if ($inputDescricao.value) $inputDescricao.setAttribute('disabled', '')
 
         $form.querySelector('#infos-exercicios').classList.add('d-none')
 
         criarInfoExercicio();
-
-        salvarExercicios();
-        console.log(exercicios)
 
         $infosExercicios.forEach((info) => { info.textContent = '' });
         $inputsExercicios.forEach((input) => { input.value = '' });
@@ -192,11 +187,11 @@ function salvarExercicios() {
     lis.forEach(li => {
         const series = li.querySelector('[data-info="series"]');
         const repeticoes = li.querySelector('[data-info="repeticoes"]');
-        const carga = li.querySelector('[data-info="carga"]');
+        const carga = li.querySelector('[data-info="carga"]'); 
         const duracao = li.querySelector('[data-info="duracao"]');
         const intervalo = li.querySelector('[data-info="intervalo"]');
         const observacao = li.querySelector('[data-info="observacao"]');
-
+        
         const exercicio = {
             id: id(exercicios),
             nome: `${li.querySelector('[data-info="nome"]').innerHTML}`,
@@ -207,32 +202,31 @@ function salvarExercicios() {
         if (duracao) exercicio.duracao = duracao.innerHTML;
         if (intervalo) exercicio.intervalo = intervalo.innerHTML;
         if (observacao) exercicio.observacao = observacao.innerHTML;
-
+        
         exercicios.push(exercicio);
     })
-    updateLocalStorageExercicios();
 }
 
 function criarInfoExercicio() {
     const $li = document.createElement('li');
     const nome = `<span data-info='nome'>${$inputNomeExercicio.value}</span>`
-
-    const series = `${$inputSeries.value ?
+    
+    const series = `${$inputSeries.value ? 
         ` <span data-info='series'>${$inputSeries.value}</span> <i class="fa-solid fa-xmark xmark-icon"></i> ` : ``}`;
-
-    const reps = `${$inputRepeticoes.value ?
+    
+    const reps = `${$inputRepeticoes.value ? 
         ` <span data-info='repeticoes'>${$inputRepeticoes.value}</span> rep. ` : ``}`
-
-    const carga = `${$inputCarga.value ?
+    
+    const carga = `${$inputCarga.value ? 
         ` <i class="fa-solid fa-dumbbell dumbbell-icon"></i> <span data-info='carga'>${$inputCarga.value}</span> kg ` : ``}`;
-
-    const duracao = `${$inputDuracao.value ?
+    
+    const duracao = `${$inputDuracao.value ? 
         ` <i class="fa-solid fa-stopwatch stopwatch-icon"></i> <span data-info='duracao'>${$inputDuracao.value}</span> seg. ` : ``}`;
 
-    const intervalo = `${$inputIntervalo.value ?
+    const intervalo = `${$inputIntervalo.value ? 
         ` <i class="fa-solid fa-hand hand-icon"></i> <span data-info='intervalo'>${$inputIntervalo.value}</span> seg.` : ``}`;
-
-    const observacao = `${$inputObservacao.value ?
+        
+    const observacao = `${$inputObservacao.value ? 
         `<i class="fa-solid fa-triangle-exclamation triangle-exclamation-icon"></i> 
         <span data-info='observacao' class="small">${$inputObservacao.value}</span>` : ``}`;
 
@@ -250,37 +244,27 @@ function criarInfoExercicio() {
             `;
     $li.querySelector('#delete').addEventListener('click', function () {
         this.parentNode.parentNode.parentNode.remove();
-        salvarExercicios();
-        console.log(exercicios.length)
-        // se o usuário deletar todos os exercicios
-        if (exercicios.length === 0) {
-            localStorage.setItem('treino', JSON.stringify(''));
-            localStorage.setItem('descricao', JSON.stringify(''));
-            $form.querySelector('#infos-treino').classList.add('d-none');
-            $olExercicios.classList.add('d-none');
-            $inputNomeTreino.removeAttribute('disabled', '');
-            $inputDescricao.removeAttribute('disabled', '');
-            $inputNomeTreino.value = '';
-            $inputDescricao.value = '';
-        }
     })
     $olExercicios.appendChild($li);
 
 }
 
 $btnSalvarTreino.addEventListener('click', () => {
-    salvarExercicios();
+    salvarExercicios()
     if (exercicios.length !== 0) {
-        salvarTreino();
-        criarTreinos(treinos);
-        $allInfos.forEach((info) => info.textContent = '');
-        $allInputs.forEach((input) => input.value = '');
-        exercicios = [];
-        $olExercicios.innerHTML = '';
-        $inputNomeTreino.removeAttribute('disabled', '')
-        $inputDescricao.removeAttribute('disabled', '')
-        $form.querySelector('#infos-treino').classList.add('d-none')
-        $form.querySelector('#infos-exercicios').classList.add('d-none')
+        const resposta = confirm('Deseja salvar o treino?');
+        if (resposta === true) {
+            salvarTreino();
+            criarTreinos(treinos);
+            $allInfos.forEach((info) => info.textContent = '');
+            $allInputs.forEach((input) => input.value = '');
+            exercicios = [];
+            $olExercicios.innerHTML = '';
+            $inputNomeTreino.removeAttribute('disabled', '')
+            $inputDescricao.removeAttribute('disabled', '')
+            $form.querySelector('#infos-treino').classList.add('d-none')
+            $form.querySelector('#infos-exercicios').classList.add('d-none')
+        }
     } else {
         alert("Não há exercícios salvos!")
     }
@@ -295,11 +279,7 @@ function salvarTreino() {
     }
     exercicios.forEach((ex) => treino.exercicios.push(ex));
     treinos.push(treino);
-    exercicios = [];
-    updateLocalStorageExercicios();
-    updateLocalStorageTreinos();
-    localStorage.setItem('treino', JSON.stringify(''));
-    localStorage.setItem('descricao', JSON.stringify(''));
+    updateLocalStorage(treinos);
 }
 
 function novoId() {
@@ -315,9 +295,25 @@ function criarTreinos(treinos) {
         const $divExercicios = $divTreino.querySelector('.card-body-treino');
         $divExercicios.innerHTML = Exercicios({ ...treino });
 
+        // const $btnEditar = $divExercicios.querySelectorAll('.btn-editar-treino');
+        // $btnEditar.forEach(btn => btn.addEventListener('click', function() {
+        //     const element = this.parentNode.parentNode.parentNode.parentNode; 
+        //     const $modal = element.querySelector('#exercicioDialog')
+        //     const $btnCancelar = element.querySelector('#cancelar');
+        //     const $btnConfirmar = element.querySelector('#confirmar');
+        //     $btnCancelar.addEventListener('click', function () {
+        //         $modal.close();
+        //     });
+        //     $btnConfirmar.addEventListener('click', function () {
+        //         const $inputs = $modal.querySelectorAll('input');
+        //         console.log($inputs)
+        //     })
+        //     $modal.showModal()
+        // }))
+
         $treinos.appendChild($divTreino);
         $divTreino.querySelector('.btnApagarTreino').addEventListener('click', function () {
-            if (confirm(`Deseja apagar ${treino.nome}?`)) {
+            if(confirm(`Deseja apagar ${treino.nome}?`)) {
                 const element = this.parentNode.parentNode.parentNode;
                 apagarTreino(element)
                 apagarBtnTreino(element)
@@ -326,92 +322,9 @@ function criarTreinos(treinos) {
     })
 }
 
-function renderizarInfo() {
-    if (exercicios.length > 0) {
-
-        $form.querySelector('#infos-treino').classList.remove('d-none');
-        
-        const nomeTreino = JSON.parse(localStorage.getItem('treino'));
-        $inputNomeTreino.value = nomeTreino;
-        $inputNomeTreino.setAttribute('disabled', '')
-        $infoNomeTreino.innerHTML = `Nome: <span class="info-value">${nomeTreino}</span>`
-
-        const descricao = JSON.parse(localStorage.getItem('descricao'));
-        if(descricao) {
-            $inputDescricao.value = descricao;
-            $inputDescricao.setAttribute('disabled', '');
-            $infoDescricao.innerHTML = `Descrição: <span class="info-value">${descricao}</span>`;
-        }
-
-        $form.querySelector('#infos-treino').classList.remove('d-none')
-        $olExercicios.classList.remove('d-none');
-        exercicios.forEach(exercicio => {
-
-            const $li = document.createElement('li');
-            const nome = `<span data-info='nome'>${exercicio.nome}</span>`
-
-            const series = `${exercicio.series ?
-                ` <span data-info='series'>${exercicio.series}</span> <i class="fa-solid fa-xmark xmark-icon"></i> ` : ``}`;
-
-            const reps = `${exercicio.repeticoes ?
-                ` <span data-info='repeticoes'>${exercicio.repeticoes}</span> rep. ` : ``}`
-
-            const carga = `${exercicios.carga ?
-                ` <i class="fa-solid fa-dumbbell dumbbell-icon"></i> <span data-info='carga'>${exercicios.carga}</span> kg ` : ``}`;
-
-            const duracao = `${exercicio.duracao ?
-                ` <i class="fa-solid fa-stopwatch stopwatch-icon"></i> <span data-info='duracao'>${exercicio.duracao}</span> seg. ` : ``}`;
-
-            const intervalo = `${exercicio.intervalo ?
-                ` <i class="fa-solid fa-hand hand-icon"></i> <span data-info='intervalo'>${exercicio.intervalo}</span> seg.` : ``}`;
-
-            const observacao = `${exercicio.observacao ?
-                `<i class="fa-solid fa-triangle-exclamation triangle-exclamation-icon"></i> 
-                                    <span data-info='observacao' class="small">${exercicio.observacao}</span>` : ``}`;
-
-            $li.innerHTML = `
-                <div class="row align-items-center">
-                    <div class="col">
-                        ${nome}${series || reps || carga ? '<br>' : ''}${series}${reps}${carga}
-                        ${duracao || intervalo ? '<br>' : ''}${duracao}${intervalo}
-                        ${observacao ? `<br>${observacao}` : ''}
-                    </div>
-                    <div class="col-2">
-                        <i class="fa-solid fa-delete-left delete-icon" id="delete"></i>
-                    </div>
-                </div>
-            `;
-            $li.querySelector('#delete').addEventListener('click', function () {
-                this.parentNode.parentNode.parentNode.remove();
-                salvarExercicios();
-                console.log(exercicios.length)
-                // se o usuário deletar todos os exercicios
-                if (exercicios.length === 0) {
-                    localStorage.setItem('treino', JSON.stringify(''));
-                    localStorage.setItem('descricao', JSON.stringify(''));
-                    $form.querySelector('#infos-treino').classList.add('d-none');
-                    $olExercicios.classList.add('d-none');
-                    $inputNomeTreino.removeAttribute('disabled', '');
-                    $inputDescricao.removeAttribute('disabled', '');
-                    $inputNomeTreino.value = '';
-                    $inputDescricao.value = '';
-                }
-
-            })
-            $olExercicios.appendChild($li);
-        })
-        exercicios = [];
-    } 
-}
-
-function updateLocalStorageTreinos() {
+function updateLocalStorage(treinos) {
     localStorage.setItem('treinos', JSON.stringify(treinos));
 }
-
-function updateLocalStorageExercicios() {
-    localStorage.setItem('exercicios', JSON.stringify(exercicios));
-}
-
 
 function apagarBtnTreino(element) {
     element.remove();
@@ -420,5 +333,6 @@ function apagarBtnTreino(element) {
 function apagarTreino(element) {
     const id = element.classList.item(1).split('-')[1];
     treinos = treinos.filter(element => element.id != id);
-    updateLocalStorageTreinos();
+    updateLocalStorage(treinos)
 }
+
